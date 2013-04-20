eae26804392c4d7db2eea9f61cadca86766ae11c
Oliver <oliver.behncke@haw-hamburg.de>
2013-04-20 13:40:46 +0200

    Applied new format rules

--- src/test/java/de/haw_hamburg/server/RepliesTest.java
+++ src/test/java/de/haw_hamburg/server/RepliesTest.java
@@ -9,13 +9,16 @@ public class RepliesTest {
 	@Test
 	public void testReplyFromString() {
 		assertEquals(Replies.ok(), Replies.replyFromString(OkReply.okString()));
-		
-		String message = "Somemessage"; 
-		assertEquals(Replies.ok(message), Replies.replyFromString(OkReply.okString() + message));
-		
-		assertEquals(Replies.error(), Replies.replyFromString(ErrorReply.errorString()));
-		
-		assertEquals(Replies.error(message), Replies.replyFromString(ErrorReply.errorString() + message));
+
+		String message = "Somemessage";
+		assertEquals(Replies.ok(message),
+				Replies.replyFromString(OkReply.okString() + message));
+
+		assertEquals(Replies.error(),
+				Replies.replyFromString(ErrorReply.errorString()));
+
+		assertEquals(Replies.error(message),
+				Replies.replyFromString(ErrorReply.errorString() + message));
 	}
 
 }
